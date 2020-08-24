60be64146e4d5d5c7f825c5d54a4073818c318fe
Ann-Marie <anniedillier@gmail.com>
2020-08-23 19:59:30 -0700

    local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,69 +2,103 @@
 var curDay = moment().format('LLLL');
 $("#currentDay").text(curDay);
 
-// loop rows
-//var time = $("#time")
-//for (var i = 0; i>(time.length); i++){
-    //get the time and place in section
-    var timeBlock = 9
-    $("#time").text(timeBlock + ":00");
+var timeBlock = 9
+$("#time").append(timeBlock + ":00");
 
-//};
-var appts = {};
+var eventIdCounter = 0;
+var events = [];
 
-var createAppt = function(appText) {
-    // create elements that make up a app item
-    var apptDiv = $("<div>").addClass("textarea");
-    var app = $("<div>").addClass("textarea").text(appText)
-    // append
-    apptDiv.append(appt);
+var editEvent = function(eventText) {
+    // create elements that make up a event item
+    var eventInput = $("<p>").addClass("textarea");
   
+    // add id as a custom attribute
+    events.setAttribute = "event-id", eventIdCounter;
+
+    console.log(eventInput);
     // check due date
-    //auditApp(apptDiv);
+    //auditEvent(EventDiv);
   
     // append to div list on the page
-    $("#textarea" + apptList).append(apptDiv);
-  };
+    $("p").append(eventInput);
+    saveEvents()
 
+    eventIdCounter++;
+};
 
-//task text was clicked
-$(".textarea").on("click", function() {
-    // get current text of p element
+var loadEvents = function() {
+    events = JSON.parse(localStorage.getItem("event"));
+
+    // if nothing in localStorage, create a new object to track all task status arrays
+    if (!events) {
+        events = {
+        events:  []
+        }
+    }
+};
+  
+var saveEvents = function() {
+    localStorage.setItem("event", JSON.stringify(events));
+};
+
+//event text was clicked
+$(".eventInput").on("click", "p", function() {
+    // get current text of the textarea
     var text = $(this)
         .text()
         .trim();
-
-    // replace p element with a new textarea
+  
+      // replace the textarea with a new textarea
     var textInput = $("<textarea>")
-        .addClass("col-sm-10 textarea past")
+        .addClass("col-sm-10 textarea")  
         .val(text);
         $(this).replaceWith(textInput);
         textInput.trigger("focus");
-  
+
 });
+
+$(".eventInput").on("blur", "textarea", function() {
+    // get the textarea's current value/text
+    var text = $(this)
+        .val()
+        .trim();
+
+    // get the task's position in the list of other li elements
+    var index = $(this)
+        .closest(".eventInput-item")
+        .index();
+
+    // update task in array and re-save to localstorage
+    events[index] = text;
+    saveEvents();
+    console.log(text)
+
+      // recreate p element
+    var eventP = $("<textarea>")
+    .addClass("col-sm-10 eventInput past")
+    .text(text);
+
+    // replace textarea with p element
+    $(this).replaceWith(eventP);
+    console.log(eventP)
+    
+  });
 
 $(".saveBtn").on("click", function() {
-    var loadAppts = function() {
-        appts = JSON.parse(localStorage.getItem("appts"));
-    
-        // if nothing in localStorage, create a new object to track all task status arrays
-        if (!appts) {
-        appts = [];
-        }
-    
-        // loop over object properties
-        $.each(appts, function(list, arr) {
-        console.log(list, arr);
-        // then loop over sub-array
-        arr.forEach(function(app) {
-            createTask(appt.text);
-            });
-        });
-    }
-  
-});
-var saveAppt = function() {
-    localStorage.setItem("appts", JSON.stringify(appt));
-}
+    // get current text of textarea
+    editEvent()
+     
+     console.log("button was clicked");
+    });
 
-loadAppts();
+    // update event in array and re-save to localstorage
+     events.push({
+        text: eventInput
+      });
+    eventIdCounter++;
+
+    
+saveEvents();
+
+
+loadEvents()
